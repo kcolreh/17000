e94dfa0d4cfed79b1d80d917976bf0f8ebd54938
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-12-22 12:46:58 +0100

    Create a function that lets Jenny pick a date and calculates time estimations based on it

--- love-java.js
+++ love-java.js
@@ -1,10 +1,11 @@
 const heartBtn = document.querySelector('.heart-btn');
 const container = document.querySelector('#heart-container');
+const enterDateBtn = document.querySelector('#enter-date')
 
 let btnscore = 0;
 
 heartBtn.addEventListener('click', createHeart);
-
+enterDateBtn.addEventListener('click', pickDate)
 
 
 function createHeart() {
@@ -29,7 +30,7 @@ function inSeconds(d1, d2) {
     let t2 = d2.getTime();
     let t1 = d1.getTime();
     let result = parseInt((t2 - t1) / 1000);
-    let resultDisplay = document.getElementById('seconds');
+    const resultDisplay = document.getElementById('seconds');
     resultDisplay.textContent = result;
 }
 
@@ -37,7 +38,7 @@ function inMinutes(d1, d2) {
     let t2 = d2.getTime();
     let t1 = d1.getTime();
     let result = parseInt((t2 - t1) / (1000 * 60));
-    let resultDisplay = document.getElementById('minutes');
+    const resultDisplay = document.getElementById('minutes');
     resultDisplay.textContent = result;
 }
 
@@ -45,7 +46,7 @@ function inHours(d1, d2) {
     let t2 = d2.getTime();
     let t1 = d1.getTime();
     let result = parseInt((t2 - t1) / (1000 * 60 * 60));
-    let resultDisplay = document.getElementById('hours');
+    const resultDisplay = document.getElementById('hours');
     resultDisplay.textContent = result;
 }
 
@@ -53,7 +54,7 @@ function inDays(d1, d2) {
     let t2 = d2.getTime();
     let t1 = d1.getTime();
     let result = parseInt((t2 - t1) / (24 * 3600 * 1000));
-    let resultDisplay = document.getElementById('days');
+    const resultDisplay = document.getElementById('days');
     resultDisplay.textContent = result;
 }
 
@@ -61,7 +62,7 @@ function inWeeks(d1, d2) {
     let t2 = d2.getTime();
     let t1 = d1.getTime();
     let result = parseInt((t2 - t1) / (24 * 3600 * 1000 * 7));
-    let resultDisplay = document.getElementById('weeks');
+    const resultDisplay = document.getElementById('weeks');
     resultDisplay.textContent = result;
 }
 
@@ -71,15 +72,49 @@ function inMonths(d1, d2) {
     let d1M = d1.getMonth();
     let d2M = d2.getMonth();
     let result = (d2M + 12 * d2Y) - (d1M + 12 * d1Y);
-    let resultDisplay = document.getElementById('months');
+    const resultDisplay = document.getElementById('months');
     resultDisplay.textContent = result; 
 }
 
 function inYears(d1, d2) {
     let result = d2.getFullYear() - d1.getFullYear();
-    let resultDisplay = document.getElementById('years');
+    const resultDisplay = document.getElementById('years');
     resultDisplay.textContent = result;
 }
+
+
+function pickDate(){
+    let dString = prompt("Babu, please enter the date (August, 24, 2019)");
+    let d1 = new Date(dString);
+    let d2 = new Date();
+    let d1Y = d1.getFullYear();
+    let d2Y = d2.getFullYear();
+    let d1M = d1.getMonth();
+    let d2M = d2.getMonth();
+    let t2 = d2.getTime();
+    let t1 = d1.getTime();
+    let resultSeconds = parseInt((t1 - t2) / 1000);
+    let resultMinutes = parseInt((t1 -t2) / (1000 * 60));
+    let resultHours = parseInt((t1 - t2) / (1000 * 60 * 60));
+    let resultDays = parseInt((t1 - t2) / (24 * 3600 * 1000));
+    let resultWeeks = parseInt((t1 - t2) / (24 * 3600 * 1000 * 7));
+    let resultMonths = (d1M + 12 * d1Y) - (d2M + 12 * d2Y) ;
+
+    const resultDisplaySeconds = document.getElementById('seconds-container3');
+    const resultDisplayMinutes = document.getElementById('minutes-container3');
+    const resultDisplayHours = document.getElementById('hours-container3');
+    const resultDisplayDays = document.getElementById('days-container3');
+    const resultDisplayWeeks = document.getElementById('weeks-container3');
+    const resultDisplayMonths = document.getElementById('months-container3');
+
+    resultDisplaySeconds.textContent = resultSeconds;
+    resultDisplayMinutes.textContent = resultMinutes;
+    resultDisplayHours.textContent = resultHours;
+    resultDisplayDays.textContent = resultDays;
+    resultDisplayWeeks.textContent = resultWeeks;
+    resultDisplayMonths.textContent = resultMonths;
+}
+
 
 inSeconds(d1, d2);
 inMinutes(d1, d2);
